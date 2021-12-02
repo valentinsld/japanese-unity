71815d28b83a3c64055b7e00f2d730780bd856b1
valentinsld <valentin.salaud@gmail.com>
2021-12-02 15:15:46 +0100

    feat: particule add speed

--- Japanese-unity/Assets/Particules/ParticulesCircle.cs
+++ Japanese-unity/Assets/Particules/ParticulesCircle.cs
@@ -17,6 +17,8 @@ public class ParticulesCircle : MonoBehaviour
 
     // PATH
     public float Speed = 0.3f;
+    public float MaxSpeed = 1.8f;
+    public float IncrementSPeed = 0.2f;
 
     public PathCreator pathEnd;
     public bool DoPath;
@@ -40,8 +42,8 @@ public class ParticulesCircle : MonoBehaviour
 
             RenderObjects[i].transform.localScale = new Vector3(ScaleObjects, ScaleObjects, ScaleObjects);
             RenderObjects[i].transform.localRotation = Quaternion.Euler(Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f), Random.Range(0.0f, 360.0f));
-            
-            RenderObjectsPosition[i] = ((float)i / (float)Count) * (float)endCirclePath;
+
+            RenderObjectsPosition[i] = ((float)i / (float)Count) * (float)endCirclePath * 2.0f;
             RenderObjectsVector[i] = new Vector3(Random.Range(0, 0.2f), Random.Range(0, 0.2f), Random.Range(0, 0.2f));
         }
     }
@@ -49,13 +51,22 @@ public class ParticulesCircle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (DoPath)
+        {
+            Speed = Mathf.Min(Speed + Time.deltaTime * IncrementSPeed, MaxSpeed);
+        }
+
 
         for (int i = 0; i < Count; i++)
         {
             RenderObjectsPosition[i] += Speed * Time.deltaTime;
-            if (!DoPath) RenderObjectsPosition[i] = RenderObjectsPosition[i] % endCirclePath;
+            if (!DoPath) {
+              RenderObjectsPosition[i] = RenderObjectsPosition[i] % endCirclePath;
+            }
             RenderObjects[i].transform.position = pathEnd.path.GetPointAtDistance(RenderObjectsPosition[i], PathCreation.EndOfPathInstruction.Stop);
             RenderObjects[i].transform.position += RenderObjectsVector[i];
+            float ownScale = ScaleObjects * (1f - Mathf.Clamp( (RenderObjectsPosition[i] - endPath * 0.9f) / (endPath - endPath * 0.9f), 0f, 1.0f ));
+            RenderObjects[i].transform.localScale = new Vector3(ownScale, ownScale, ownScale);
         }
     }
 }
