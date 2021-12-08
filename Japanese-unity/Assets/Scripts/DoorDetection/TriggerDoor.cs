02eb3f304e32e21507bc01161e89d8f6cd6089df
Dan-Hen <leo.clot16@gmail.com>
2021-12-09 00:15:16 +0100

    add basic door animation and script

---
+++ Japanese-unity/Assets/Scripts/DoorDetection/TriggerDoor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerDoor : MonoBehaviour
+{
+
+    [SerializerField] private Animator myDoorController
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("player"))
+        myDoorController.SetBool("playSpin2" ; true)
+    }
+}
